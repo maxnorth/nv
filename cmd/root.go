9b19329268f6769685810787537b94c4fcb97bb2
Max North <maxwelldnorth@gmail.com>
2024-02-04 00:09:10 -0500

    Add dir target support

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"errors"
+	"fmt"
+	"os"
+	"path"
 
 	"github.com/spf13/cobra"
 )
@@ -9,6 +12,16 @@ import (
 func NewRootCmd() *cobra.Command {
 	rootCmd := &cobra.Command{
 		Use: "nv",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			dir := cmd.Flag("dir").Value.String()
+			workDir, _ := os.Getwd()
+			dirPath := path.Join(workDir, dir)
+			if err := os.Chdir(dirPath); err != nil {
+				// TODO: other kinds of errors? permission issues?
+				return fmt.Errorf("target directory not found: %s", dirPath)
+			}
+			return nil
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			// if -- not in args
 			if cmd.ArgsLenAtDash() == -1 {
@@ -25,6 +38,8 @@ func NewRootCmd() *cobra.Command {
 		},
 		SilenceUsage: true,
 	}
+
+	rootCmd.PersistentFlags().StringP("dir", "d", ".", "path to dir containing nv.yaml and .env files")
 	rootCmd.PersistentFlags().StringP("env", "e", "local", "target environment")
 
 	rootCmd.AddCommand(
